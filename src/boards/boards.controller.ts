d91384e170c3882705b18c34bbabf2cb4fa5adb3
daekun0920 <dosemfb0920@gmail.com>
2021-10-04 19:46:36 +0900

    # 게시물 생성하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -10,5 +10,13 @@ export class BoardsController {
     @Get('/')
     getAllBoard(): Board[] {
         return this.boardService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body('title') title: string,
+        @Body('description') description: string
+    ): Board {
+        return this.boardService.createBoard(title, description);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
     // 인 메모리 데이터 베이스
     private boards: Board[] = [];
 
+    // 메소드명: 리턴타입
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, description: string): Board {
+        const board: Board = {
+            id: uuid(),
+            title, // title: title
+            description, // description: description
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }
