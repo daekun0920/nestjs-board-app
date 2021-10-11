fd3f3c4abea9832e86d8d11e708fdc40b6c73b42
daekun0920 <dosemfb0920@gmail.com>
2021-10-11 12:32:17 +0900

    # 유저 데이터 유효성 체크 및 유저 이름 유니크 검사

--- src/auth/auth-credential.dto.ts
+++ src/auth/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: 'only English and numbers are accepted for password.'    
+    })
     password: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./auth-credential.dto";
 import { User } from "./user.entity";
@@ -8,7 +9,15 @@ export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password })
-        await this.save(user);
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('User name already exists.');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 
 }
